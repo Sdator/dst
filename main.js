c5519283d0f43607ccc6eb3f1394b0d5a2071408
liusikair <250740270@qq.com>
2019-12-19 07:53:34 +0800

    1

--- main.js
+++ main.js
@@ -4,24 +4,6 @@ var $$ = mdui.JQ;
 
 
 
-
-
-
-function 格式化数据(json) {
-
-    const 蛋类 = json.FoodEggs.Foods
-    const 果类 = json.FoodFruit.Foods
-    const 肉类 = json.FoodMeats.Foods
-    const 特殊 = json.FoodNoFc.Foods
-    const 其他 = json.FoodOthers.Foods
-    const 食谱 = json.FoodRecipe.FoodRecipes
-    const 菇类 = json.FoodVegetables.Foods
-    const { FoodEggs: 测试 } = json
-
-}
-
-
-
 var thead_top = $('thead').offset().top;
 
 $(window).scroll(function () {
@@ -50,25 +32,111 @@ $(window).scroll(function () {
 })
 
 
-
+// <td>{{ 食物.Name }} </td>
+// <td>{{ 食物.Health }} </td>
+// <td>{{ 食物.Hunger }} </td>
+// <td>{{ 食物.Sanity }} </td>
+// <td>{{ 食物.Perish }} </td>
+// <td>{{ 食物.Cooktime }} </td>
+// <td>{{ 食物.Priority }} </td>
 
 const app = new Vue({
     el: "#app",
     data: {
         食谱: [],
-        表头食物属性: ["名字", "生命", "饥饿", "精神", "保鲜", "烹饪时间", "优先度"],
+        排序: [],
+        searchTextA: "",
+        msg: "",
+        num: 0,
+        表头食物属性: ["序列", "名字", "生命", "饥饿", "精神", "保鲜", "烹饪时间", "优先度"],
 
     },
     created() {
         fetch('/data/DS/Foods.json').then(function (response) {
             return response.json();
         }).then(json => {
-            this.食谱 = json.FoodRecipe.FoodRecipes
+
+            this.食谱 = this.格式化数据(json)
+
             console.log(this.食谱)
+
+            // for (const { Name, Health, Hunger, Sanity, Perish, Cooktime, Priority } of json.FoodRecipe.FoodRecipes) {
+            //     let item = Health + 100
+            //     // console.log(Health + 100)
+            //     setTimeout((排序, data) => {
+
+            //         排序.push(data)
+
+            //     }, item, this.排序, {
+            //         "名字": Name,
+            //         "生命": Health,
+            //         "饥饿": Hunger,
+            //         "精神": Sanity,
+            //         "保鲜": Perish,
+            //         "烹饪时间": Cooktime,
+            //         "优先度": Priority
+            //     })
+            // }
+            // 睡眠排序
+            // for (const 属性 of json.FoodRecipe.FoodRecipes) {
+            //     let item = 属性.Health + 100
+            //     setTimeout((食谱, data) => {
+            //         食谱.push(data)
+            //     }, item, this.食谱, 属性)
+            // }
+            // console.log(this.食谱, [...this.食谱])
         }).catch(function (ex) {
             console.log('Parsing failed:', ex);
         });
 
+    },
+    methods: {
+        格式化数据(json) {
+
+            const 蛋类 = json.FoodEggs.Foods
+            const 果类 = json.FoodFruit.Foods
+            const 肉类 = json.FoodMeats.Foods
+            const 特殊 = json.FoodNoFc.Foods
+            const 其他 = json.FoodOthers.Foods
+            const 食谱 = json.FoodRecipe.FoodRecipes
+            const 菇类 = json.FoodVegetables.Foods
+
+            return [...蛋类, ...果类, ...肉类, ...特殊, ...其他, ...食谱, ...菇类]
+
+        },
+
+        // 排序开始(data) {
+        //     排序
+        // }
+        // 代入值，正倒叙
+        cli(e, model) {
+            // 清零
+            this.排序 = []
+            this.num = 0
+
+            const 对照表 = {
+                "名字": "Name",
+                "生命": "Health",
+                "饥饿": "Hunger",
+                "精神": "Sanity",
+                "保鲜": "Perish",
+                "烹饪时间": "Cooktime",
+                "优先度": "Priority"
+            }
+            const 名字 = 对照表[e]
+
+            // 睡眠排序
+            for (const 属性 of this.食谱) {
+                let item = model ? ~(属性[名字] - 100) : 属性[名字] + 100
+                console.log(item)
+                setTimeout((排序, data) => {
+                    排序.push(data)
+                }, item, this.排序, 属性)
+            }
+            this.食谱 = this.排序
+            // console.log(this.排序)
+
+        }
     }
 
 })
